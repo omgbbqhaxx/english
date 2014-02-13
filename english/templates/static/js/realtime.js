f0a2be127f7481350a16132b3e6ecd481c055f84
Yasin Aktimur <chardone@windowslive.com>
2014-02-13 19:13:23 +0200

    onmessage history için değişti

--- english/templates/static/js/realtime.js
+++ english/templates/static/js/realtime.js
@@ -1,8 +1,7 @@
-
          function connect(){
          function displayMessage(message){
          $('#msjs div:lt(-4)').remove();
-         $("#msjs").prepend(message);
+         $("#msjs").append(message);
          
          }
 
@@ -13,13 +12,11 @@ displayMessage('');
 
 sockJS.onmessage = function(event) {
                     event.data = JSON.parse(event.data);
-                    var xxuser = event.data.user;
-                    var xxdogri = event.data.dogru;
-                    var xxhatali = event.data.hatali;
-                    var xximg = event.data.img;
-                    var xxlink = event.data.link;
-                    displayMessage('<div style="width:390px;" id="xnyy"  class="row"><div style="left:5px;"  class="col-lg-12"><section class="comment-list block"><article id="comment-id-1" class="comment-item media arrow arrow-left"><a class="pull-left thumb-small avatar"><img src="http://graph.facebook.com/'+xximg+'/picture?type=normal" class="img-circle"></a><section class="media-body panel"><header class="panel-heading clearfix"><a href="'+xxlink+'">'+ xxuser +'</a><br>'+ xxdogri +' kelime bildi ve '+ xxhatali +' yeni kelime öğrendi.</header></section></article></section></div></div>');
-                    
+                    console.log(event.data);
+                    event.data.forEach(function(entry) {
+                    displayMessage('<div style="width:390px;" id="xnyy"  class="row"><div style="left:5px;"  class="col-lg-12"><section class="comment-list block"><article id="comment-id-1" class="comment-item media arrow arrow-left"><a class="pull-left thumb-small avatar"><img src="http://graph.facebook.com/'+entry[0]+'/picture?type=normal" class="img-circle"></a><section class="media-body panel"><header class="panel-heading clearfix"><a href="'+entry[1]+'">'+ entry[2] +'</a><br>'+ entry[3] +' kelime bildi ve '+ entry[4] +' yeni kelime öğrendi.</header></section></article></section></div></div>');
+                    });
+                   
                     
 };
 sockJS.onclose = function() {
