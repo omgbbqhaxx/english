cb32f290607399ae4031cc815db6e0e79ab78c64
Yasin Aktimur <chardone@windowslive.com>
2014-02-13 19:14:24 +0200

    console logsilindi

--- english/templates/static/js/realtime.js
+++ english/templates/static/js/realtime.js
@@ -12,7 +12,6 @@ displayMessage('');
 
 sockJS.onmessage = function(event) {
                     event.data = JSON.parse(event.data);
-                    console.log(event.data);
                     event.data.forEach(function(entry) {
                     displayMessage('<div style="width:390px;" id="xnyy"  class="row"><div style="left:5px;"  class="col-lg-12"><section class="comment-list block"><article id="comment-id-1" class="comment-item media arrow arrow-left"><a class="pull-left thumb-small avatar"><img src="http://graph.facebook.com/'+entry[0]+'/picture?type=normal" class="img-circle"></a><section class="media-body panel"><header class="panel-heading clearfix"><a href="'+entry[1]+'">'+ entry[2] +'</a><br>'+ entry[3] +' kelime bildi ve '+ entry[4] +' yeni kelime öğrendi.</header></section></article></section></div></div>');
                     });
